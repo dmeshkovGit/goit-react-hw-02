5a4ec9639b658a59ece4f94302f7171630e9aabc
dmeshkovGit <140300183+dmeshkovGit@users.noreply.github.com>
2024-02-20 02:05:53 +0200

    task completed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,62 @@
-import { useState } from 'react'
-import styles from './App.module.css'
-import { Profile } from '../Profile/Profile'
-import { TaskTitle } from '../TaskTitle/TaskTitle'
-import { FriendList } from '../FriendList/FriendList'
-import { TransactionHistory } from '../TransactionHistory/TransactionHistory'
-import userData from '../../userData.json'
-import friendsList from '../../friendsList.json'
-import transactions from '../../transactions.json'
+import { useState, useEffect } from 'react'
+import css from './App.module.css'
+import Description from '../Description/Description'
+import Options from '../Options/Options'
+import Feedback from '../Feedback/Feedback'
+import Notification from '../Notification/Notification'
 
-export const App = () => {
+const getInitFeedback = () => {
+  const initFeedback = window.localStorage.getItem("feedback-data")
+  return ((initFeedback !== null) ? JSON.parse(initFeedback) : { good: 0, neutral: 0, bad: 0 })
+};
+
+export default function App() {
+
+  const [feedback, setFeedback] = useState(getInitFeedback());
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  const isFeedback = (totalFeedback > 0)
+
+  const updateFeedback = (feedbackType) => {
+    switch (feedbackType) {
+      case "good":
+        setFeedback({ ...feedback,
+            good: feedback.good + 1})
+        break;
+    
+      case "bad":
+        setFeedback({ ...feedback,
+            bad: feedback.bad + 1})
+        break;
+      
+      case "neutral":
+        setFeedback({ ...feedback,
+            neutral: feedback.neutral + 1})
+        break;
+      
+       case "reset":
+        setFeedback({
+          good: 0,
+          neutral: 0,
+          bad: 0
+        })
+        break;
+    }
+  };
+
+  useEffect(() => {
+window.localStorage.setItem("feedback-data",JSON.stringify(feedback))
+  }, [feedback])
+
+
   return (
     <>
-      <TaskTitle>Task №1</TaskTitle>
-      <Profile userData={userData}/>
-      <hr />
-      <TaskTitle>Task №2</TaskTitle>
-      <FriendList friends={friendsList} />
-      <hr />
-      <TaskTitle>Task №3</TaskTitle>
-      <TransactionHistory transactions={transactions} />
-      <hr />
+      <Description />
+      <Options onUpdate={updateFeedback} isFeedback={isFeedback} />
+      {(isFeedback) ?
+        <Feedback feedback={feedback} totalFeedback={totalFeedback} /> 
+        : <Notification>No feedback yet</Notification>}
     </>
   )
 }
--- src/components/TaskTitle/TaskTitle.jsx
+++
@@ -1,7 +0,0 @@
-import css from './TaskTitle.module.css'
-
-export const TaskTitle = ({children}) => {
-    return (
-        <h2 className={css.title}>{children}</h2>
-    )
-}
